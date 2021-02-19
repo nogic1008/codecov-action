cba6d784d42392c517f7efe907a98be7edb83758
Nogic <24802730+nogic1008@users.noreply.github.com>
2021-02-19 17:16:54 +0900

    test: reset environment variables

--- src/buildExec.test.ts
+++ src/buildExec.test.ts
@@ -1,21 +1,9 @@
 import buildExec from './buildExec';
 
-test('no arguments', () => {
-  const {execArgs, filepath, failCi} = buildExec();
-  expect(execArgs).toEqual([
-    'codecov.sh',
-    '-n',
-    '',
-    '-F',
-    '',
-    '-Q',
-    'github-action',
-  ]);
-  expect(filepath).toEqual('codecov.sh');
-  expect(failCi).toBeFalsy();
-});
+describe('buildExec', () => {
+  /** Environment variables before testing */
+  const storedEnv = process.env;
 
-test('all arguments', () => {
   const envs = {
     'move_coverage_to_trash': 'true',
     'commit_parent': '83231650328f11695dfb754ca0f540516f188d27',
@@ -50,89 +38,118 @@ test('all arguments', () => {
     'xcode_package': 'MyApp',
   };
 
-  for (const env of Object.keys(envs)) {
-    process.env['INPUT_' + env.toUpperCase()] = envs[env];
-  }
+  // Clear INPUT_*** env variables
+  beforeEach(() => {
+    jest.resetModules();
+    process.env = {...storedEnv};
+    for (const env of Object.keys(envs)) {
+      process.env['INPUT_' + env.toUpperCase()] = undefined;
+    }
+  });
+  // Restore env
+  afterAll(() => {
+    process.env = storedEnv;
+  });
 
-  const {execArgs, filepath, failCi} = buildExec();
-  expect(execArgs).toEqual([
-    'src/codecov.sh',
-    '-n',
-    'codecov',
-    '-F',
-    'test',
-    '-Q',
-    'github-action',
-    '-c',
-    '-N',
-    '83231650328f11695dfb754ca0f540516f188d27',
-    '-A',
-    '--timeout 1',
-    '-U',
-    '--timeout 2',
-    '-e',
-    'OS,PYTHON',
-    '-Z',
-    '-f',
-    'coverage.xml',
-    '-f',
-    'dir1/coverage.xml',
-    '-f',
-    'dir2/coverage.xml',
-    '-X',
-    'gcov',
-    '-X',
-    'coveragepy',
-    '-X',
-    'fix',
-    '-X',
-    'search',
-    '-X',
-    'code',
-    '-X',
-    'network',
-    '-X',
-    'gcovout',
-    '-X',
-    'html',
-    '-X',
-    'recursesubs',
-    '-a',
-    '--timeout 3',
-    '-g',
-    '**/exclude-dir/*.*',
-    '-x',
-    'gcov',
-    '-G',
-    '**/include-dir/*.*',
-    '-k',
-    'demo',
-    '-B',
-    'thomasrockhu/test',
-    '-b',
-    '1',
-    '-C',
-    '9caabca5474b49de74ef5667deabaf74cdacc244',
-    '-P',
-    '2',
-    '-T',
-    'v1.2',
-    '-N',
-    'root/',
-    '-s',
-    'coverage/',
-    '-v',
-    '-q',
-    'codecov/',
-    '-D',
-    '~/Library/Developer/Xcode/DerivedData',
-    '-J',
-    'MyApp',
-  ]);
-  expect(filepath).toEqual('src/codecov.sh');
-  expect(failCi).toBeTruthy();
+  test('no arguments', () => {
+    const {execArgs, filepath, failCi} = buildExec();
 
-  for (const env of Object.keys(envs)) {
-    delete process.env['INPUT_' + env.toUpperCase()];
-  }
+    expect(execArgs).toEqual([
+      'codecov.sh',
+      '-n',
+      '',
+      '-F',
+      '',
+      '-Q',
+      'github-action',
+    ]);
+    expect(filepath).toEqual('codecov.sh');
+    expect(failCi).toBeFalsy();
+  });
+
+  test('all arguments', () => {
+    for (const env of Object.keys(envs)) {
+      process.env['INPUT_' + env.toUpperCase()] = envs[env];
+    }
+
+    const {execArgs, filepath, failCi} = buildExec();
+
+    expect(execArgs).toEqual([
+      'src/codecov.sh',
+      '-n',
+      'codecov',
+      '-F',
+      'test',
+      '-Q',
+      'github-action',
+      '-c',
+      '-N',
+      '83231650328f11695dfb754ca0f540516f188d27',
+      '-A',
+      '--timeout 1',
+      '-U',
+      '--timeout 2',
+      '-e',
+      'OS,PYTHON',
+      '-Z',
+      '-f',
+      'coverage.xml',
+      '-f',
+      'dir1/coverage.xml',
+      '-f',
+      'dir2/coverage.xml',
+      '-X',
+      'gcov',
+      '-X',
+      'coveragepy',
+      '-X',
+      'fix',
+      '-X',
+      'search',
+      '-X',
+      'code',
+      '-X',
+      'network',
+      '-X',
+      'gcovout',
+      '-X',
+      'html',
+      '-X',
+      'recursesubs',
+      '-a',
+      '--timeout 3',
+      '-g',
+      '**/exclude-dir/*.*',
+      '-x',
+      'gcov',
+      '-G',
+      '**/include-dir/*.*',
+      '-k',
+      'demo',
+      '-B',
+      'thomasrockhu/test',
+      '-b',
+      '1',
+      '-C',
+      '9caabca5474b49de74ef5667deabaf74cdacc244',
+      '-P',
+      '2',
+      '-T',
+      'v1.2',
+      '-N',
+      'root/',
+      '-s',
+      'coverage/',
+      '-v',
+      '-q',
+      'codecov/',
+      '-D',
+      '~/Library/Developer/Xcode/DerivedData',
+      '-J',
+      'MyApp',
+    ]);
+    expect(filepath).toEqual('src/codecov.sh');
+    expect(failCi).toBeTruthy();
+  });
 });
+
